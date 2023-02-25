a725d2ed7576aaf7e991f2e3cbda5f64004ed7d0
Vadim Khamatov <74698624+vrkhamatov@users.noreply.github.com>
2023-02-25 22:40:29 +0300

    feat(BackendService): make primary version of backendService

--- BackendService/src/main/java/com/example/backendservice/controller/BackendController.java
+++ BackendService/src/main/java/com/example/backendservice/controller/BackendController.java
@@ -1,33 +1,27 @@
 package com.example.backendservice.controller;
 
 import com.example.backendservice.service.BackendService;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @RestController
 public class BackendController {
 
-    BackendService backendService;
+    BackendService backendService = new BackendService();
 
+    @Autowired
+    Environment env; //get path to file directory from command line
+
+    // command: //java -jar target/BackendService-0.0.1-SNAPSHOT.jar --path=C:\Users\khama\Desktop\LetiPrint\BackendService\src\main\resources\storage
     @GetMapping("/downloadFile/{filename}")
-    private ResponseEntity<?> getFile(@PathVariable String filename) throws FileNotFoundException {
-        File file = new File("C:\\Users\\khama\\Desktop\\storage\\test.txt");
-        InputStreamResource i = new InputStreamResource(new FileInputStream(file));
-            return ResponseEntity.ok()
-                    .header("fileName",file.getName())
-                    .contentType(MediaType.parseMediaType("application/octet-stream"))
-                    .body(i);
+    private ResponseEntity<?> getFile(@PathVariable String filename) {
+        String envResponse = env.getProperty("path");
+        return backendService.getFileFromLocalDirectory(envResponse, filename);
     }
 
 }
--- BackendService/src/main/java/com/example/backendservice/service/BackendService.java
+++ BackendService/src/main/java/com/example/backendservice/service/BackendService.java
@@ -1,10 +1,38 @@
 package com.example.backendservice.service;
 
 
+import com.example.backendservice.AppError;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 @Service
 public class BackendService {
+
+
+
+    public ResponseEntity<?> getFileFromLocalDirectory(String envResponse, String filename) {
+        File file = new File(envResponse + "\\" + filename);
+        InputStreamResource i;
+        try {
+            i = new InputStreamResource(new FileInputStream(file)); //response if file was founded
+            return ResponseEntity.ok()
+                    .header("fileName", file.getName())
+                    .contentType(MediaType.parseMediaType("application/octet-stream"))
+                    .body(i);
+        } catch (FileNotFoundException e) { //response if file wasn't founded
+            return new ResponseEntity<>(new AppError(HttpStatus.NOT_FOUND.value(),
+                    "Product with filename " + filename + " not found"),
+                    HttpStatus.NOT_FOUND);
+        }
+
+    }
 
 
 }
