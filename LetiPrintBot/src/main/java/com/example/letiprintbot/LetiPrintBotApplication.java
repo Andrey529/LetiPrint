fa913fdf6efb156801fc9baa0d21ab3736a7c595
Vadim Khamatov <74698624+vrkhamatov@users.noreply.github.com>
2023-03-21 18:55:55 +0300

    feat(LetiPrintBot): Initial commit

---
+++ LetiPrintBot/src/main/java/com/example/letiprintbot/LetiPrintBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.letiprintbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetiPrintBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetiPrintBotApplication.class, args);
+    }
+
+}
